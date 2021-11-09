67b2c6f69b972028048841ba66d47982c4878943
Ankit <ankit.escalate360@gmail.com>
2021-11-09 19:54:40 +0530

    on click outside and click esc close modal

--- modal/index.js
+++ modal/index.js
@@ -1,11 +1,27 @@
 const open = document.getElementById("open");
 const modalContainer = document.getElementById("modal_container");
+const modal = document.getElementById("modal");
 const close = document.getElementById("close");
 
-open.addEventListener("click", () => {
-  modalContainer.classList.add("show");
+open.addEventListener("click", () => showModal());
+
+close.addEventListener("click", () => hideModal());
+
+modalContainer.addEventListener("click", () => hideModal());
+
+modal.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  e.stopImmediatePropagation();
+  return false;
 });
 
-close.addEventListener("click", () => {
-  modalContainer.classList.remove("show");
+document.addEventListener("keydown", (e) => {
+  const keyCode = e.key;
+  if (keyCode === "Escape") {
+    hideModal();
+  }
 });
+
+const hideModal = () => modalContainer.classList.remove("show");
+const showModal = () => modalContainer.classList.add("show");
